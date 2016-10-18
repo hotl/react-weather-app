10448d7ba8122f27e75da62af5984c1e444bb921
Timothy Ho <timothyho0820@gmail.com>
2016-10-18 10:12:18 -0700

    Prep repo for Heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ var express = require('express');
 
 // Create our app
 var app = express();
-var port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    // call next to move on to next process once this one completes
+    if (req.headers['x-forwarded-proto'] === 'http') {
+      next();
+    } else {
+      console.log('hostname: ', req.hostname);
+      console.log('url: ', req.url);
+      res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 app.listen(port, function() {
   console.log("Express server is listening at port " + port);
