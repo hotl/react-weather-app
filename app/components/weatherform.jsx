f82d5e73f2f317380ca52a642dc9f92feef4e66b
Timothy Ho <timothyho0820@gmail.com>
2016-10-18 21:25:53 -0700

    Adding Styling to Weather pages

--- app/components/weatherform.jsx
+++ app/components/weatherform.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
       <div>
         <form onSubmit={this.onFormSubmit}>
           <input type="text" ref="location"/>
-          <button className="hollow button">Get Weather</button>
+          <button className="hollow expanded button">Get Weather</button>
         </form>
       </div>
     )
--- app/components/weathermessage.jsx
+++ app/components/weathermessage.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var WeatherMessage = ({temp, location}) => {
   return (
-    <h1>It is {temp} degrees in {location}</h1>
+    <h1 className="text-center">It is {temp} degrees in {location}</h1>
   );
 }
 
