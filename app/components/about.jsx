b3867555293be3e051eb0fe0e56e0b84be4591ab
Timothy Ho <timothyho0820@gmail.com>
2016-10-18 21:05:44 -0700

    Styled About page with Foundation

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -2,7 +2,30 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About Component</h3>
+    <div>
+      <h1 className="text-center">About Page</h1>
+      <p>This is a weather application built on React.
+      I built this to familiarize myself with the basic
+      features offered by React, as well as to practice
+      my chops in web development.</p>
+      <p>Feel free to check out the source code on my
+      <a href="https://github.com/hotl/react-weather-app"> GitHub!</a>
+      </p>
+      <p>
+        Here are some of the tools that I used:
+        <ul>
+          <li>
+            <a href="http://facebook.github.io/react">React.js</a>
+            - JavaScript library used
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a>
+            - Used to fetch weather data
+          </li>
+        </ul>
+      </p>
+    </div>
+
   );
 };
 
